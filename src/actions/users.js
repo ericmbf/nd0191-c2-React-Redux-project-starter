4c6a58a683a781f61c2c6c1db08d0ef07f95995a
Eric Costa Hall <ericmbf@gmail.com>
2022-08-07 19:03:55 -0400

    Fix user answer question

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,18 @@
 export const RECEIVE_USERS = "RECEIVE_USERS"
+export const ANSWER_FROM_USER = "ANSWER_FROM_USER"
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users: users
+    };
+}
+
+export function answerUserQuestion(info) {
+    return {
+        type: ANSWER_FROM_USER,
+        authedUser: info.authedUser, 
+        qid: info.qid,
+        answer: info.answer
     };
 }
