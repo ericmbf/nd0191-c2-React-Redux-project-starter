fb0e2a8362c075d476ac67cad147497d91fcf69e
Eric Costa Hall <ericmbf@gmail.com>
2022-08-08 21:10:34 -0400

    Fix test dom using fireEvent

--- src/components/newQuestion.test.js
+++ src/components/newQuestion.test.js
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import QuestionCard from './QuestionCard';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NewQuestion from './newQuestion';
+import reducer from "../reducers"
+import middleware from "../middleware"
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 describe('NewQuestion', () => {
     it('will display an error if the answers input is not provided.', () => {
-        
+        const store = createStore(reducer, middleware);
+
         var component = render(
-            <Router>
-                <NewQuestion />
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <NewQuestion />
+                </Router>
+            </Provider>
         );
 
         var submitButton = component.getByTestId('submit-button');
